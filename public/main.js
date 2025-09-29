4fcc183071bff6adafff4550145cfdd2aa6d205b
Mikaela Matthews <mikaelamatthews@MacBookAir.attlocal.net>
2025-09-29 16:09:32 -0500

    completed hide/icon for js

--- public/main.js
+++ public/main.js
@@ -1,1 +1,18 @@
-//change icons from hidden to show when pressed
+// Change input from password to text
+
+const hideImg = document.getElementById('hide')
+const passwordInput = document.getElementById('password')
+
+hide.addEventListener("click", changing)
+
+function changing() {
+    if(hideImg.src.includes('images/hide.png')) {
+        hideImg.src = "images/show.png"
+        passwordInput.type = "text"
+    }
+
+    else {
+        hideImg.src = "images/hide.png"
+        passwordInput.type = "password"
+    }
+}
